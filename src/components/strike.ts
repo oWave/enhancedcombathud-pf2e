037c5100b3c483701eeeb1113659ee4aaf1c3519
oWave <oWave@users.noreply.github.com>
2023-12-05 22:43:23 +0100

    WIP

--- src/components/strike.ts
+++ src/components/strike.ts
@@ -1,9 +1,11 @@
 import { ActorPF2e, CharacterPF2e } from "types/pf2e/src/module/actor"
 import { StrikeData } from "types/pf2e/src/module/actor/data/base"
 import { WeaponPF2e } from "types/pf2e/src/module/item"
-import { ToggleWeaponTraitParams } from "types/pf2e/src/module/item/weapon/helpers"
+import { toggleWeaponTrait as toggleWeaponTraitType } from "types/pf2e/src/module/item/weapon/helpers"
 import { DamageType } from "types/pf2e/src/module/system/damage"
 import { StrikeRuleElement } from "types/pf2e/src/module/rules/rule-element/strike"
+
+type ToggleWeaponTraitParams = Parameters<typeof toggleWeaponTraitType>[0]
 
 export class StrikeButton extends CONFIG.ARGON.CORE.ArgonComponent {
   strike: StrikeData
@@ -16,6 +18,8 @@ export class StrikeButton extends CONFIG.ARGON.CORE.ArgonComponent {
     return {
       label: this.strike.label,
       icon: this.strike.item.img,
+      hasConfig: !!this.strike.versatileOptions.length,
+      selectedVersatileOption: this.strike.versatileOptions.find((e) => e.selected),
       versatileOptions: this.strike.versatileOptions,
       buttons: this.strike.variants.map((v, i) => {
         return {
@@ -32,12 +36,12 @@ export class StrikeButton extends CONFIG.ARGON.CORE.ArgonComponent {
   }
 
   get classes() {
-    return ["action-element", "ech-blur"]
+    return []
   }
 
   get visible() {
     // This is false for the basic unarmed attack when configured to hide that attack in the actors Configure dialog
-    return this.strike.visible
+    return ((<ActorPF2e>this.actor).type == "character" && this.strike.visible) || true
   }
 
   activateListeners() {
@@ -49,50 +53,48 @@ export class StrikeButton extends CONFIG.ARGON.CORE.ArgonComponent {
       })
     )
 
-    this.element.querySelectorAll<HTMLElement>(".strikes-list").forEach((row) => {
-      const versatileToggleButtons = row.querySelectorAll<HTMLButtonElement>("button[data-action=toggle-versatile]",);
-      for (const button of versatileToggleButtons) {
-        button.addEventListener("click", () => {
-          const weapon = this.strike.item as WeaponPF2e<ActorPF2e>;
-          const baseType = weapon?.system?.damage.damageType ?? null;
-          const selection =
-            button.classList.contains("selected") || button.value === baseType ? null : button.value as DamageType;
-          const selectionIsValid = selection === null || selection in CONFIG.PF2E.damageTypes;
-          if (weapon && selectionIsValid) {
-            toggleWeaponTrait({ trait: "versatile", weapon, selection });
-            this.updateStrike();
-          }
-        });
-      }
+    this.element.querySelector(".ech-strike-config-header")?.addEventListener("click", () => {
+      this.element.querySelector(".ech-strike-config")?.classList.add("open")
     })
-  }
+    this.element.querySelector(".ech-strike-config-content .collapse")?.addEventListener("click", () => {
+      this.element.querySelector(".ech-strike-config")?.classList.remove("open")
+    })
 
-  updateStrike() {
-    setTimeout(() => {
-      this.strike = this.actor.system.actions?.find(b => b.item === this.strike.item) || this.strike;
-      this.render();
-    }, 500);
+    this.element.querySelectorAll<HTMLButtonElement>(".versatile-options button").forEach((button) =>
+      button.addEventListener("click", async () => {
+        const weapon = this.strike.item as WeaponPF2e<ActorPF2e>
+        const baseType = weapon?.system?.damage.damageType ?? null
+        const selection =
+          button.classList.contains("selected") || button.value === baseType ? null : (button.value as DamageType)
+        const selectionIsValid = selection === null || selection in CONFIG.PF2E.damageTypes
+        if (weapon && selectionIsValid) {
+          await toggleWeaponTrait({ trait: "versatile", weapon, selection })
+          this.strike =
+            (this.actor as ActorPF2e).system.actions?.find((e) => e.item.id == this.strike.item.id) || this.strike
+          this.render()
+        }
+      })
+    )
   }
 }
 
 async function toggleWeaponTrait({ weapon, trait, selection }: ToggleWeaponTraitParams): Promise<boolean> {
-  const current = weapon.system.traits.toggles[trait].selection;
-  if (current === selection) return false;
+  const current = weapon.system.traits.toggles[trait].selection
+  if (current === selection) return false
 
-  const item = weapon.actor?.items.get(weapon.id);
+  const item = weapon.actor?.items.get(weapon.id)
   if (item?.isOfType("weapon") && item === weapon) {
-    await item.update({ [`system.traits.toggles.${trait}.selection`]: selection });
+    await item.update({ [`system.traits.toggles.${trait}.selection`]: selection })
   } else if (item?.isOfType("weapon") && weapon.altUsageType === "melee") {
-    item.update({ [`system.meleeUsage.traitToggles.${trait}`]: selection });
+    await item.update({ [`system.meleeUsage.traitToggles.${trait}`]: selection })
   } else if (trait === "versatile" && item?.isOfType("shield")) {
-    item.update({ "system.traits.integrated.versatile.selection": selection });
+    await item.update({ "system.traits.integrated.versatile.selection": selection })
   } else {
     const rule = item?.rules.find(
-      (r): r is StrikeRuleElement => r.key === "Strike" && !r.ignored && r.slug === weapon.slug,
-    );
-    await rule?.toggleTrait({ trait, selection });
+      (r): r is StrikeRuleElement => r.key === "Strike" && !r.ignored && r.slug === weapon.slug
+    )
+    await rule?.toggleTrait({ trait, selection })
   }
 
-  return true;
+  return true
 }
-
