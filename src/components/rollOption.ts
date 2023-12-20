9d2777b82e383e1bd40b8b2b8982abf841973706
oWave <oWave@users.noreply.github.com>
2023-12-20 14:34:29 +0100

    Strike config and RollOptions

--- src/components/rollOption.ts
+++ src/components/rollOption.ts
@@ -1,11 +1,19 @@
+import { ActorPF2e } from "types/pf2e/src/module/actor"
 import { RollOptionToggle } from "types/pf2e/src/module/rules/synthetics"
 
 export class RollOptionButton extends CONFIG.ARGON.CORE.ArgonComponent {
   toggle: RollOptionToggle
+  expanded: boolean
   constructor(toggle: RollOptionToggle) {
     super()
 
     this.toggle = toggle
+    this.expanded = false
+
+    ui.ARGON.hooks.add("updateItem", toggle.itemId, async () => {
+      this.toggle = (this.actor as ActorPF2e).synthetics.toggles.find((e) => e.option == this.toggle.option)!
+      await this.render()
+    })
   }
 
   async getData() {
@@ -13,6 +21,11 @@ export class RollOptionButton extends CONFIG.ARGON.CORE.ArgonComponent {
 
     return {
       ...this.toggle,
+      icon: !this.toggle.enabled
+        ? "fa-regular fa-square-minus"
+        : this.toggle.checked
+        ? "fa-solid fa-square"
+        : "fa-regular fa-square",
       hasSuboptions: this.toggle.suboptions.length > 0,
       suboptions,
       selected: this.toggle.suboptions.find((e) => e.selected),
@@ -25,7 +38,33 @@ export class RollOptionButton extends CONFIG.ARGON.CORE.ArgonComponent {
 
   activateListeners() {
     this.element.querySelector(".expand")?.addEventListener("click", () => {
+      this.expanded = !this.expanded
       this.element.querySelector(".expandable")?.classList.toggle("open")
     })
+
+    this.element.querySelectorAll<HTMLDivElement>(".expandable [data-value]").forEach((e) =>
+      e.addEventListener("click", async () => {
+        const value = e.dataset.value
+        if (!value) return
+        this.expanded = false
+        this.setSuboption(value)
+        // Rerendered via hook
+      })
+    )
+
+    this.element.querySelector(".toggle")?.addEventListener("click", async (e) => {
+      if (this.toggle.enabled) return this.setActive(!this.toggle.checked)
+      // Rerendered via hook
+    })
+  }
+
+  async setActive(state: boolean) {
+    const { domain, option, itemId } = this.toggle
+    return (this.actor as ActorPF2e).toggleRollOption(domain, option, itemId, state, null)
+  }
+
+  async setSuboption(value: string) {
+    const { domain, option, itemId, checked } = this.toggle
+    return (this.actor as ActorPF2e).toggleRollOption(domain, option, itemId, checked, value)
   }
 }
